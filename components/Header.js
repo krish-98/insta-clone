287ef5032f309e020b3af6062cc055d761addc87
krish-98 <muralikrishnanm98@gmail.com>
2022-05-06 21:18:37 +0530

    header section completed

---
+++ components/Header.js
@@ -0,0 +1,74 @@
+import Image from 'next/image'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
+
+const Header = () => {
+  return (
+    <div className="sticky top-0 z-50 border-b bg-white shadow-sm">
+      <div className="mx-5 flex max-w-6xl justify-between lg:mx-auto">
+        {/* Left */}
+        <div className="relative hidden w-24 cursor-pointer lg:inline-grid">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="relative w-10 flex-shrink-0 cursor-pointer lg:hidden">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+            // priority="true"
+          />
+        </div>
+
+        {/* Middle - Search input field */}
+        <div className="max-w-xs">
+          <div className="relative mt-1 rounded-md p-3">
+            <div className="absolute inset-y-0 flex items-center pl-3  ">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="block w-full rounded-md border-gray-300 bg-gray-50 pl-10 focus:border-black focus:ring-black sm:text-sm"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 cursor-pointer md:hidden" />
+
+          <div className="navBtn relative">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute -top-1 -right-2  flex h-5 w-5 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+
+          <img
+            className="h-10 cursor-pointer rounded-full"
+            src="https://muralikrishnan.netlify.app/assets/pro.7a7ac16e591417165bb41b19aca52620.jpg"
+            alt="profile pic"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
