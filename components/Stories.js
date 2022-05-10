6e4b9c8d25e21f3c4c7ed19f52d79a43e7b526de
krish-98 <muralikrishnanm98@gmail.com>
2022-05-10 14:17:32 +0530

    loggedin user's story icon added

--- components/Stories.js
+++ components/Stories.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker'
 
 import Story from './Story'
+import { useSession } from 'next-auth/react'
 
 const Stories = () => {
   const [suggestions, setSuggestions] = useState([])
+  const { data: session } = useSession()
 
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
@@ -18,6 +20,10 @@ const Stories = () => {
 
   return (
     <div className="mt-8 flex space-x-2 overflow-x-scroll rounded-sm border border-gray-200 bg-white p-6 scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
+
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
